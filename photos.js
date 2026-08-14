746d25567927e782570212f2c0bc32fe0df198bc
Misha Zinovyev <zmd@gosniias.ru>
2026-08-14 18:37:55 +0300

    new report

--- photos.js
+++ photos.js
@@ -239,6 +239,391 @@ const ALL_PHOTOS = [
     "title": "Москворецкий пойменный заказник"
   },
   {
+    "img": "travel/sorolansaari-2026/thumbs/00.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/01.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/02.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/10.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/11.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/12.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/13.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/14.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/20.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/21.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/22.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/23.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/30.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/31.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/32.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/40.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/41.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/42.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/50.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/51.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/52.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/53.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/54.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/60.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/61.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/62.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/63.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/64.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/65.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/66.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/67.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/70.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/71.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/72.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/73.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/74.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/75.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/76.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/77.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/78.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/80.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/81.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/82.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/83.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/84.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/85.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/86.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/87.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/88.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/89.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/90.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/91.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/92.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/100.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/101.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/102.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/103.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/104.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/105.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/106.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/107.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/108.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/109.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/110.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/111.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/112.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/113.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/120.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/130.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/131.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/132.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/133.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/134.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/135.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/136.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/810.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
+    "img": "travel/sorolansaari-2026/thumbs/811.jpg",
+    "link": "travel/sorolansaari-2026/sorolansaari-2026.html",
+    "title": "Поход по Соролансаари"
+  },
+  {
     "img": "travel/UAE-2024/thumbs/00.jpg",
     "link": "travel/UAE-2024/UAE-2024.html",
     "title": "Небольшой очерк об ОАЭ (Рас-Эль-Хайма)"
--- travel/squeeze.ps1
+++ travel/squeeze.ps1
@@ -2,31 +2,48 @@ Add-Type -AssemblyName System.Drawing
 
 $maxSide = 800
 $quality = 75L
-
 $encoder = [System.Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() | Where-Object { $_.MimeType -eq 'image/jpeg' }
 $encParams = New-Object System.Drawing.Imaging.EncoderParameters(1)
 $encParams.Param[0] = New-Object System.Drawing.Imaging.EncoderParameter([System.Drawing.Imaging.Encoder]::Quality, $quality)
+
+function Fix-Orientation {
+    param($img)
+    $orientationId = 0x0112
+    if ($img.PropertyIdList -contains $orientationId) {
+        $prop = $img.GetPropertyItem($orientationId)
+        $orientation = $prop.Value[0]
+        switch ($orientation) {
+            2 { $img.RotateFlip([System.Drawing.RotateFlipType]::RotateNoneFlipX) }
+            3 { $img.RotateFlip([System.Drawing.RotateFlipType]::Rotate180FlipNone) }
+            4 { $img.RotateFlip([System.Drawing.RotateFlipType]::Rotate180FlipX) }
+            5 { $img.RotateFlip([System.Drawing.RotateFlipType]::Rotate90FlipX) }
+            6 { $img.RotateFlip([System.Drawing.RotateFlipType]::Rotate90FlipNone) }
+            7 { $img.RotateFlip([System.Drawing.RotateFlipType]::Rotate270FlipX) }
+            8 { $img.RotateFlip([System.Drawing.RotateFlipType]::Rotate270FlipNone) }
+        }
+        # убираем тег, чтобы вьюеры не применили поворот повторно
+        $img.RemovePropertyItem($orientationId)
+    }
+}
 
 $photoDirs = Get-ChildItem -Path '.' -Recurse -Directory | Where-Object { $_.Name -eq 'photos' }
 
 foreach ($photoDir in $photoDirs) {
     $thumbsDir = Join-Path $photoDir.Parent.FullName 'thumbs'
-    if (-not (Test-Path $thumbsDir)) {
-        New-Item -ItemType Directory -Path $thumbsDir | Out-Null
-    }
+    if (-not (Test-Path $thumbsDir)) { New-Item -ItemType Directory -Path $thumbsDir | Out-Null }
 
     Write-Host "=== Processing: $($photoDir.FullName) ==="
-
     $searchPath = Join-Path $photoDir.FullName '*'
     $files = Get-ChildItem -Path $searchPath -File -Include *.jpg, *.jpeg
 
     if (-not $files -or $files.Count -eq 0) {
-        Write-Host "  (no .jpg files found)"
+        Write-Host " (no .jpg files found)"
         continue
     }
 
     foreach ($file in $files) {
         $img = [System.Drawing.Image]::FromFile($file.FullName)
+        Fix-Orientation $img
 
         if ($img.Width -gt $maxSide -or $img.Height -gt $maxSide) {
             $ratio = [Math]::Min($maxSide / $img.Width, $maxSide / $img.Height)
@@ -45,10 +62,9 @@ foreach ($photoDir in $photoDirs) {
 
         $outPath = Join-Path $thumbsDir $file.Name
         $bmp.Save($outPath, $encoder, $encParams)
-
         $bmp.Dispose()
         $g.Dispose()
 
-        Write-Host "  $($file.Name) -> thumbs/$($file.Name)"
+        Write-Host " $($file.Name) -> thumbs/$($file.Name)"
     }
 }
